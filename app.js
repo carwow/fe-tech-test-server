8f69a85b9754909935a938cb33eab1dab5d24030
Vincent Navetat <vincent.navetat@carwow.co.uk>
2021-08-13 14:13:11 +0100

    Remove show endpoint and simplify data

--- app.js
+++ app.js
@@ -1,52 +1,24 @@
 const express = require('express');
 const port = process.env.PORT || 3000;
 const models = require('./models.json');
-const simpleModels = JSON.parse(JSON.stringify(models));
-
-simpleModels.forEach(model => {
-  delete model.available_colors;
-  delete model.recommended_engine;
-});
 
 let app = express();
 
 app.use(express.static('public'))
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
 app.get('/', function (req, res) {
- res.send(JSON.stringify({ Hello: 'World'}));
+  res.send(JSON.stringify({ models: '/models' }));
 });
 
 app.get('/models', function (req, res) {
-  res.status(200).json(simpleModels);
-});
-
-app.get('/model/:id', (req, res) => {
-  const id = parseInt(req.params.id, 10);
-
-  if (!Number.isNaN(id)) {
-    const task = models.find(item => {
-      return (item.id === id);
-    });
-
-    if (task !== null) {
-      res.status(200).json(task);
-    } else {
-      return res.status(404).json({
-        message: 'Not found.',
-      });
-    }
-  } else {
-    return res.status(400).json({
-      message: 'Bad request.',
-    });
-  }
+  res.status(200).json(models);
 });
 
 app.listen(port, function () {
- console.log(`the server is available on http://localhost:${port}/\n`);
+  console.log(`the server is available on http://localhost:${port}/\n`);
 });
